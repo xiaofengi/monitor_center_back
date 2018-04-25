7ceb76a18c5c759bf57585cc369666f7ffcf3f57
hfwang <hfwang@leqee.com>
2018-04-25 15:57:54 +0800

    fix mapper location

--- src/main/java/org/hdu/Main.java
+++ src/main/java/org/hdu/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.hdu.back.Mapper")
+@MapperScan("org.hdu.back.mapper")
 public class Main {
 
 	public static void main(String[] args) {
